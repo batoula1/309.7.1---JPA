51ef53a88a040498148dc3b83fad1aea93f82583
batoula1 <102437354+batoula1@users.noreply.github.com>
2024-09-01 00:53:12 -0400

    Second-Commit

--- src/main/java/repository/TutorialRepository.java
+++ src/main/java/repository/TutorialRepository.java
@@ -1,17 +1,18 @@
-package TutorialRepository;
+package com.example.demo.TutorialRepository;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.TutorialRepository;
+import com.example.demo.model.Tutorial;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface TutorialRepository extends JpaRepository<TutorialRepository, Long>
+public interface TutorialRepository extends JpaRepository<Tutorial, Long>
         // Tutorial → Entity type and Long --> primary key
 {
     //   findByPublished is a custom method()
-    List<TutorialRepository> findByPublished(boolean published);
+    List<Tutorial> findByPublished(boolean published);
 }
