4514e20c0b4a667b8a4d5488fd9e1ef5ce6d76d9
batoula1 <102437354+batoula1@users.noreply.github.com>
2024-09-01 01:09:17 -0400

    Lastcommit

--- src/main/java/repository/TutorialRepository.java
+++ src/main/java/repository/TutorialRepository.java
@@ -1,15 +1,11 @@
-package com.example.demo.TutorialRepository;
-
+package repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.example.demo.model.Tutorial;
-
 import java.util.List;
 import java.util.Optional;
-
 @Repository
+
 public interface TutorialRepository extends JpaRepository<Tutorial, Long>
         // Tutorial → Entity type and Long --> primary key
 {
